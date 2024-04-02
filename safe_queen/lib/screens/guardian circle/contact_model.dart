878d392c5795de81faeaa33de26f5b68eb41c263
hima <himansa2226@gmail.com>
2024-04-02 16:38:16 +0530

    profile edit ok. guardian circle delete call ok

---
+++ safe_queen/lib/screens/guardian circle/contact_model.dart
@@ -0,0 +1,7 @@
+class Contact {
+  String id;
+  String name;
+  String phoneNumber;
+
+  Contact({required this.id, required this.name, required this.phoneNumber});
+}
---
+++ safe_queen/lib/screens/profiles/profile.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:safe_queen/screens/profiles/about_us.dart';
+import 'package:safe_queen/screens/profiles/edit_profile.dart';
+import 'package:safe_queen/services/auth.dart';
+ 
+
+class Profile extends StatelessWidget {
+
+  // Create an object from AuthService
+  final AuthServices _auth = AuthServices();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 150),
+            TextButton.icon(
+              onPressed: () {
+                // Navigate to EditProfilePage
+                Navigator.push(context, MaterialPageRoute(builder: (context) => EditProfile()));
+              },
+              icon: Icon(Icons.edit), // Icon for edit profile
+              label: Text(
+                'Edit Details',
+                style: TextStyle(fontSize: 18), // Adjust font size here
+              ),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.black, padding: EdgeInsets.symmetric(vertical: 20, horizontal: 100), // Adjust height and width here
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: BorderSide(color: Colors.black), // Add border color here
+                ),
+                backgroundColor: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 20),
+            TextButton.icon(
+              onPressed: () {
+                // Navigate to EditProfilePage
+                Navigator.push(context, MaterialPageRoute(builder: (context) => EditProfile()));
+              },
+              icon: Icon(Icons.location_city), // Icon for edit profile
+              label: Text(
+                'My Location',
+                style: TextStyle(fontSize: 18), // Adjust font size here
+              ),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.black, padding: EdgeInsets.symmetric(vertical: 20, horizontal: 95), // Adjust height and width here
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: BorderSide(color: Colors.black), // Add border color here
+                ),
+                backgroundColor: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 20),
+            TextButton.icon(
+              onPressed: () {
+                // Navigate to AboutUsPage
+                Navigator.push(context, MaterialPageRoute(builder: (context) => AboutUs()));
+              },
+              icon: Icon(Icons.info), // Icon for about us
+              label: Text(
+                'About us',
+                style: TextStyle(fontSize: 18), // Adjust font size here
+              ),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.black, padding: EdgeInsets.symmetric(vertical: 20, horizontal: 107), // Adjust height and width here
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: BorderSide(color: Colors.black), // Add border color here
+                ),
+                backgroundColor: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 20),
+            TextButton.icon(
+              onPressed: () async{
+                // Call the signOut() method from AuthService
+                await _auth.signOut();
+                // Perform sign-out action
+                // For demonstration purpose, just pop back to the previous screen
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.logout), // Icon for sign out
+              label: Text(
+                'Sign out',
+                style: TextStyle(fontSize: 18), // Adjust font size here
+              ),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.black, padding: EdgeInsets.symmetric(vertical: 20, horizontal: 107), // Adjust height and width here
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: BorderSide(color: Colors.black), // Add border color here
+                ),
+                backgroundColor: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
